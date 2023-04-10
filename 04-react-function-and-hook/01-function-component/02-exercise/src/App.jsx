0634843a7d8b99079ddcca0827965b0c0c2c0249
pottor02 <doramon_toon_5@hotmail.com>
2023-04-10 10:40:35 +0700

    Merge branch 'main' of https://github.com/kanin-kearpimy/career-changer-react

--- 04-react-function-and-hook/01-function-component/02-exercise/src/App.jsx
+++ 04-react-function-and-hook/01-function-component/02-exercise/src/App.jsx
@@ -14,6 +14,10 @@ function Header() {
   return (
     // Code here
     // <Header />
+                <header>
+                <span>Turn on / off</span>
+                <p>Current Temperature: XXX</p>
+            </header>
   );
 }
 
@@ -21,13 +25,20 @@ function Content() {
   return (
     // Code here
     // <Content />
+    <main>
+      <Temperature />
+    </main>
   );
 }
 
 function Temperature() {
+  let temperature = 30
   return (
     // Code here
     // <Temperature />
+                    <div id="temperature">
+                    <span>{ temperature } Oc</span>
+                </div>
   );
 }
 
@@ -35,6 +46,10 @@ function Footer() {
   return (
     // Code here
     // <Footer />
+                <footer>
+                <button>Up</button>
+                <button>Down</button>
+            </footer>
   );
 }
 
