82d39af90851b83d1973effff6dbac3745f2b842
pottor02 <doramon_toon_5@hotmail.com>
2023-04-12 11:34:11 +0700

    06 route

--- 06-react-router/03-exercise/src/Nav.jsx
+++ 06-react-router/03-exercise/src/Nav.jsx
@@ -3,6 +3,8 @@ const Navbar = () => {
     return (
         <ul>
             <li><a href={'/'}>Main</a></li>
+            <li><a href={'/'}>Second</a></li>
+            <li><a href={'/'}>Third</a></li>
         </ul>
     )
 }
